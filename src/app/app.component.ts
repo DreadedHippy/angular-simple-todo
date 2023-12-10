091a1de4c60b0c8def49be1d48584e44ffa775d2
onotaizee@gmail.com <onotaizee@gmail.com>
2023-12-10 17:46:58 +0100

    feat: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
+  todoInputContent = '';
   title = 'sample-todo';
+
+  todos = signal<Todo[]>([
+    {
+      content: "Test",
+      completed: false
+    }, {
+      content: "iowfwiofjwiofjwioefjwioefjwioefjiejfiowejfiwoefjwioejfiowejfiwojefiwefjiwefjiwefjiwefjiwefjiweofjiweofjweiofjiwefjioweefjweiofjwioefjweiof",
+      completed: false
+    }
+  ]);
+
+  addTodo() {
+    console.log(this.todoInputContent);
+    if (this.todoInputContent.length < 1) {
+      return
+    }
+
+    this.todos.update(v => {
+      v.push({
+        content: this.todoInputContent,
+        completed: false
+      });
+
+      return v;
+    })
+
+    this.todoInputContent = "";
+  }
+
+  deleteTodo(index: number) {
+    this.todos.mutate(v => {
+      v.splice(index, 1);
+
+      return v;
+    })
+  }
+}
+
+
+interface Todo {
+  content: string,
+  completed: boolean
 }
